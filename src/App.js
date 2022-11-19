5d2854d87f4afa57d2264f838a95594a131bd259
prathamdubey <prathamd67@gmail.com>
2022-11-19 18:43:02 +0530

    Tailwind and Header Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Header />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="px-32 py-6 bg-blue-500 text-white">
+      <h1 className="text-[28px] font-semibold">Where in the world?</h1>
+    </nav>
+  );
+};
+
+export default Header;
